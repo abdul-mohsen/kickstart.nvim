c34b88bd0e4077d8b9140389ba1e8fe99cdadcd8
abdul-mohsen <abdh357@gmail.com>
2025-03-31 05:34:19 +0300

    some changes

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -64,6 +64,27 @@ end)
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 4
 vim.opt.makeprg = 'sh build.sh > log 2> error &'
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+  end,
+})
+
+vim.keymap.set('n', '<leader>st', function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd 'J'
+  vim.api.nvim_win_set_height(0, 5)
+  Job_id = vim.bo.channel
+  vim.fn.chansend(Job_id, { 'sh build.sh \n\r' })
+end)
+
+vim.keymap.set('n', '<leader>b', function()
+  vim.fn.chansend(Job_id, { 'sh build.sh \n\r' })
+end)
+
 return {
   -- Git related plugins
 }
